542d400f8d2ee49798bcc8928afb540e7cc9adec
AmtiX <107303548+AmtixDev@users.noreply.github.com>
2024-01-14 22:04:48 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const countConversions = require('./index');
+
+const probotCredits = 1000; // Replace with the actual number of probot credits
+
+const propalAmount = countConversions(probotCredits);
+
+console.log('Propal Amount:', propalAmount);
+
+
+// Developed by AmtiXDev
+// Discord Username: amtix
---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Count the number of conversions based on the provided formula.
+ * @param {number} probotCredits - The number of probot credits.
+ * @returns {number} - The propal amount calculated using the formula.
+ */
+function countConversions(probotCredits) {
+    return probotCredits / 20;
+  }
+  
+module.exports = countConversions;
+  
+// Developed by AmtiXDev
+// Discord Username: amtix
